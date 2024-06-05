f9d8ce451785f0db4ff5035cc320d9b93b34b177
Iago Lima Toledo <iagoltoledo@gmail.com>
2024-06-05 12:06:09 -0300

    Update and rename competencias.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/* scripts.js */
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init();
+
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+});
